4af6113433e8b351f69bd9023088b7b402ea6621
Woochul <crepers12@hanmail.net>
2021-02-16 14:25:46 +0900

    create app

--- reference-sources/voice-translator/voice-translator-config.js
+++
@@ -1,3 +0,0 @@
-var bucketName = 'REPLACE WITH VALUE of VoiceTranslatorBucket KEY';
-var IdentityPoolId = 'REPLACE WITH VALUE of IdentityPoolIdOutput KEY';
-var lambdaFunction = 'REPLACE WITH VALUE of VoiceTranslatorLambda KEY';
